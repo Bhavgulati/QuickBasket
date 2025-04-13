ed151751a4f01d3fa48156b034def48b73314754
Anurag12698 <137909157+Anurag12698@users.noreply.github.com>
2025-04-13 14:00:03 +0530

    Created Basic Structure of Routes Created routes structure and stored them in routes folder Seperated their implementation in contollers folder for better readability

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,17 @@
+//file name is auth.route.js as a common convention for route files
+
+import express from "express";
+import {login,logout,signup} from "../controllers/auth.controller.js";
+const router = express.Router();
+
+router.get("/signup", signup);
+
+
+router.get("/login", login);
+
+
+router.get("/logout", logout);
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+//routes
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes)
+
 app.listen(PORT, () => {
-    console.log("Server is Running on htpp://localhost:" + PORT);
+    console.log("Server is Running on http://localhost:" + PORT);
 })
