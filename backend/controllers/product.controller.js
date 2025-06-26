8bfab173e09a3239689bf0bd0c49517fe60105f0
Anurag <anuragsingh12698@gmail.com>
2025-06-26 16:24:42 +0530

    Added Middleware for path protection

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,7 +1,10 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get("/",getAllProducts)
-
-export default router;
+export const getAllProducts = async (req, res) => {
+  // Logic to get all products
+  try{
+    const products = await Product.find({});
+    res.json({products});
+  }catch(error){
+    console.log("Error in Fetching Products",error.message);
+    res.status(500).json({message:"Server error",error: error.message});
+  }
+}
