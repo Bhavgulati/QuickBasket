03ee098364f200a18529bc08f09628bdbe461ef2
Anurag12698 <137909157+Anurag12698@users.noreply.github.com>
2025-07-10 16:30:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,10 @@ app.use("/api/coupons",couponRoutes);
 app.use("/api/payments", paymentroutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(Path2D.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req,res)=>{
-        res.sendFile(Path2D.resolve(__dirname, "frontend","dist","index.html")); 
+        res.sendFile(path.resolve(__dirname, "frontend","dist","index.html")); 
     });
 }
 
